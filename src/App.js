b24e921bb7b9274cf5ccc4840980adf02971ce44
Suhani Sharma <suhanis2001@gmail.com>
2023-02-12 21:39:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Switch , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from './components/Home'
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Reviews from "./components/Reviews";
 import Gallery from "./components/Gallery";
+import { Outlet } from "react-router-dom";
 // import StudyMaterial from "./components/StudyMaterial";
 // import Login from "./components/Login";
 // import Payment from "./components/Payment";
@@ -17,31 +18,39 @@ import Gallery from "./components/Gallery";
 
 
 function App () {
-  return (
+  return (  
     <Router>
       <div className="App">
-        <Switch>
-        <Route path="/reviews">
+        <Routes>
+        <Route path="/reviews" element={
+          <React.Fragment>
             <Header />
             <Reviews />
             <Footer />
-          </Route>
-          <Route path="/gallery">
+          </React.Fragment>
+        } />
+          <Route path="/gallery" element={
+          <React.Fragment>
             <Header />
             <Gallery />
             <Footer />
-          </Route>
-          <Route path="/about">
+          </React.Fragment>
+        } />
+          <Route path="/about" element={
+          <React.Fragment>
             <Header />
             <About />
             <Footer />
-          </Route>
-          <Route path="/">
-            <Header/>
+          </React.Fragment>
+        } />
+          <Route path="/" element={
+          <React.Fragment>
+            <Header />
             <Home />
             <Footer />
-          </Route>
-        </Switch>
+          </React.Fragment>
+        } />
+        </Routes>
       </div>
     </Router>
   );
